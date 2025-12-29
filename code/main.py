f821330af0467896d8e633cf1a3f5dd4c8532755
matusbucher <bucher.matus@gmail.com>
2025-12-29 12:13:28 +0100

    Add baseline description length model

--- code/main.py
+++ code/main.py
@@ -3,6 +3,6 @@ import baseline_models
 
 if __name__ == "__main__":
     data = math_lighteval.data_load(normalize_labels=True)
-    baseline = baseline_models.BaselineAverageModel(data)
-    mse = baseline.evaluate()
-    print(f"Baseline Average Model MSE: {mse}")
+    baseline = baseline_models.BaselineDescriptionLengthModel(data)
+    print("Train MSE:", baseline.train_accuracy())
+    print("Test MSE:", baseline.test_accuracy())
